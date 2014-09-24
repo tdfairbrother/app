9b6e13d5725df38e36a8e62f0d83bef223cbda3b
Tim <tim@pictureboss.com>
2014-09-24 23:14:35 +1000

    Added router

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,42 @@
 /** @jsx React.DOM */
 
-var React = require('react'),
-    CommentBox = require('TF-CommentBox');
+var React = require('react');
+var CommentBox = require('TF-CommentBox');
+var ReactRouter = require('react-router-component');
 
-var Box = <CommentBox fireurl="https://burning-inferno-6221.firebaseio.com" />;
+var Pages       = ReactRouter.Pages;
+var Page        = ReactRouter.Page;
 
-module.exports = Box;
+function Comments() {
+    return <CommentBox fireurl="https://burning-inferno-6221.firebaseio.com" />
+}
+
+var App = React.createClass({
+
+    render: function() {
+        return (
+            <html>
+                <head>
+                    <script src="/assets/bundle.js" />
+                </head>
+                <Pages className="App" path={this.props.path}>
+                    <Page path="/" handler={Comments} />
+                </Pages>
+            </html>
+            );
+    }
+});
 
 if (typeof window !== 'undefined') {
     window.onload = function() {
         React.renderComponent(
-            Box,
+            App(),
             document.getElementById('content')
         );
     }
 }
 
+
+exports.App = App;
 
 
